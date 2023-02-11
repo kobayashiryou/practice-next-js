37f88677eee50446422ed82b65d1930cedff09be
小林凌 <chiri.parihead@gmail.com>
2023-02-11 14:32:51 +0900

    feat: storybookの設定を追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,32 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-p')
+const path = require('path')
+
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  "staticDirs": ['public'],
+  "babel": async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-provate-property-in-object',
+    ]
+  }),
+  "webpackFinal": async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      })
+    ];
+
+    return config
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from "@storybook/react"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { theme } from '../src/themes'
+import * as NextImage from 'next/image'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +10,47 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-  },
+  }
 }
+
+export const GlobalStyle = createGlobalStyle`
+ html,
+ body,
+ textarea {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+ }
+ * {
+  box-sizing: border-box;
+ }
+ a {
+  text-decoration: none;
+  transition: .25s;
+  color: #000000;
+ }
+`
+// Themeの適用
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {story()}
+  </ThemeProvider>
+))
+
+// storybookではnext/imageが動作しないので、通常の画像に差し替える
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
+})
+
+Object.defineProperty(NextImage, '__esModule', {
+  configurable: true,
+  value: true,
+})
