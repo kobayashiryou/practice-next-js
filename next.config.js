7769dd27288450b22bc3bffcc13f3457805baa0c
小林凌 <chiri.parihead@gmail.com>
2023-03-07 23:00:19 +0900

    feat: rewriteの設定

--- next.config.js
+++ next.config.js
@@ -14,6 +14,19 @@ const nextConfig = {
 
     return compilerConfig
   })(),
+  async rewrites() {
+    // ブラウザ側からAPIを叩くとCORSの制限に引っかかるのでrewite機能を使って、json-serverにリクエストを送る
+    return [
+      {
+        /**
+         * :matchは変数みたいなもの。その他の値でも構わない。destinationで使いまわせる
+         * APIとフロントのオリジンが別である場合にこの機能を使うと有効
+         */
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        destination: `${process.env.API_BASE_URL}/:match`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
